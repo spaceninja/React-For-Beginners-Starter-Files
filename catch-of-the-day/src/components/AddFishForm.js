884d2f9fc2ef6a899cb0f63c27cba920f217c826
Scott Vandehey <fake-scott@spaceninja.com>
2018-10-14 14:07:52 -0700

    13: understanding state

---
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  statusRef = React.createRef();
+  descRef = React.createRef();
+  imageRef = React.createRef();
+
+  createFish = e => {
+    // 1. stop form from submitting
+    e.preventDefault();
+    // 2. get the fish properties
+    const fish = {
+      name: this.nameRef.value.value,
+      price: parseFloat(this.priceRef.value.value),
+      status: this.statusRef.value.value,
+      desc: this.descRef.value.value,
+      image: this.imageRef.value.value
+    };
+    // 3. pass the new fish to the addFish method
+    this.props.addFish(fish);
+    // 4. reset the form
+    e.currentTarget.reset();
+  };
+
+  render() {
+    return (
+      <form className="fish-edit" onSubmit={this.createFish}>
+        <input name="name" ref={this.nameRef} type="text" placeholder="Name" />
+        <input
+          name="price"
+          ref={this.priceRef}
+          type="text"
+          placeholder="Price"
+        />
+        <select name="status" ref={this.statusRef}>
+          <option value="available">Fresh!</option>
+          <option value="unavailable">Sold Out!</option>
+        </select>
+        <textarea name="desc" ref={this.descRef} placeholder="Description" />
+        <input
+          name="image"
+          ref={this.imageRef}
+          type="text"
+          placeholder="Image"
+        />
+        <button>+ Add Fish</button>
+      </form>
+    );
+  }
+}
+
+export default AddFishForm;
--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,14 +4,30 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = fish => {
+    // 1. take a copy of the existing state to avoid mutation
+    const fishes = { ...this.state.fishes };
+    // 2. add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. set the new fishes object to state
+    this.setState({
+      fishes
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Inventory />
         <Order />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
